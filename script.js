4cc70456cf9ad48770929d6ece9b5f065ed6d82f
Visionner <71496647+bin599@users.noreply.github.com>
2025-01-14 15:20:34 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let targetDate1 = new Date('2025-01-06T00:00:00');
-let targetDate2 = new Date('2024-11-07T00:00:00');
+let targetDate1 = new Date('2025-06-07T00:00:00');
+let targetDate2 = new Date('2025-06-10T00:00:00');
 let targetDate = targetDate1;
 let showMilliseconds = false;
 
